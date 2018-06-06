963dc3741061315c604ed2c181c5f82b45dc90d6
Your Name <you@example.com>
2018-06-06 16:27:25 +0530

    likes_final

---
+++ src/main/java/blog/controller/RestController.java
@@ -0,0 +1,112 @@
+package blog.controller;
+
+import blog.model.Post;
+import blog.repository.PostRepository;
+import blog.repository.UserRepository;
+import com.google.common.base.Charsets;
+import com.google.common.hash.Hashing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    @Autowired
+    PostRepository postRepo;
+
+
+    @Autowired
+    UserRepository userRepo;
+
+    @PostMapping("/api/register/")
+    public String registerUser(@RequestParam("username") String uname, @RequestParam("password") String password, @RequestParam("fullname") String fullName) {
+
+        String result = String.valueOf(userRepo.findUserExist(uname));
+        String sha256hex = Hashing.sha256()
+                .hashString(password, Charsets.US_ASCII)
+                .toString();
+
+        if ((result.equalsIgnoreCase("null"))) {
+            userRepo.addUserCredentials(uname, fullName, sha256hex);
+            return uname + ", you successfully registered";
+        } else {
+            return "user already exists";
+        }
+    }
+
+    @PostMapping("/api/createpost/")
+    public String createPost(@RequestParam("username") String uname, @RequestParam("password") String password, @RequestParam("title") String title, @RequestParam("post body") String body) {
+        String passwordByUser = String.valueOf(userRepo.findUserPassword(uname));
+
+        String sha256hex = Hashing.sha256()
+                .hashString(password, Charsets.US_ASCII)
+                .toString();
+        if (!(sha256hex.equalsIgnoreCase(passwordByUser))) {
+            return "Invalid credentials";
+        }
+        Long id = System.currentTimeMillis() % 1000;
+        postRepo.addPostValues(id, body, title, uname);
+        return "Your post with title " + title + " is created";
+
+    }
+
+    @GetMapping("/api/allposts")
+    public Iterable<Post> getAllPosts() {
+        return postRepo.findAll();
+    }
+
+    @PostMapping("/api/editpost/")
+    public String editPosts(@RequestParam("id") Long postId, @RequestParam("username") String uname, @RequestParam("password") String password, @RequestParam("title") String title, @RequestParam("body") String body) {
+
+        if (postRepo.findPostById(postId) == null) {
+            return "There is no post with " + postId + " in the existing database";
+        }
+        String postOwner = postRepo.findUserByPostId(postId);
+        if (!postOwner.equalsIgnoreCase(uname))
+            return "You have no right to edit other users post";
+
+        String passwordByUser = String.valueOf(userRepo.findUserPassword(uname));
+
+        String sha256hex = Hashing.sha256()
+                .hashString(password, Charsets.US_ASCII)
+                .toString();
+        if (!(sha256hex.equalsIgnoreCase(passwordByUser))) {
+            return "Invalid credentials";
+        }
+
+        postRepo.editPostValues(body, title, postId);
+        return "the edits have been updated for the posts with new title" + title;
+    }
+    @PostMapping("/api/addLikes/")
+    public String likePost(@RequestParam("username") String uname,@RequestParam("password") String password,@RequestParam("post id") Long postId) {
+
+        if (postRepo.findPostById(postId) == null) {
+            return "There is no post with " + postId + " in the existing database";
+        }
+        String passwordByUser = String.valueOf(userRepo.findUserPassword(uname));
+
+        String sha256hex = Hashing.sha256()
+                .hashString(password, Charsets.US_ASCII)
+                .toString();
+        if (!(sha256hex.equalsIgnoreCase(passwordByUser))) {
+            return "Invalid credentials";
+        }
+
+       Long likesId = System.currentTimeMillis()%1000;
+
+        if(postRepo.checkLikes(postId, uname)==null){
+
+            postRepo.addLikes(likesId,postId,uname);
+
+            return "You liked postId "+postId+" successfully";
+        }
+        else
+            return uname + " has already liked this post";
+    }
+    @GetMapping("/api/getLikes/")
+    public Iterable<String> getAllLikes(@RequestParam("postid") int id) {
+        return  postRepo.getLikesbyPost(id);
+    }
+}
---
+++ src/main/java/blog/repository/PostRepository.java
@@ -0,0 +1,39 @@
+package blog.repository;
+
+import blog.model.Post;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+@Repository
+public interface PostRepository extends CrudRepository<Post, Integer>{
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value="INSERT INTO post (id,body,date,title,user_username) VALUES (?1,?2,NOW(),?3,?4)")
+    void addPostValues(Long id, String body, String title, String username);
+
+    @Query(nativeQuery = true,value="select user_username from post where id=?1")
+    String findUserByPostId(Long id);
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value="update post set body=?1,title=?2,date=now() where id=?3")
+    void editPostValues(String body,String title,Long id);
+
+    @Query(nativeQuery = true,value="select id from post where id=?1")
+    Long findPostById(Long id);
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value="insert into likes (id,post_id,user_username) values (?1,?2,?3)")
+    void addLikes(Long likesId,Long postId,String uname);
+
+    @Query(nativeQuery = true,value="select * from likes where post_id=?1")
+    Iterable<String> getLikesbyPost(int id);
+
+    @Query(nativeQuery = true, value="select id from likes where post_id = ?1 AND user_username=?2")
+    Long checkLikes(Long postId,String uname);
+}
